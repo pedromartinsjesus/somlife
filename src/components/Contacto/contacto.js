e27de48b08466487de345a3bb37d378db9266164
Pedro <pedromjesusti@gmail.com>
2021-08-18 20:14:31 -0300

    Adding of Contact Page

--- src/components/Contacto/contacto.js
+++ src/components/Contacto/contacto.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: 'grey',
+
+    },
+    contacto: {
+        marginLeft: 10,
+        padding: 5,
+    },
+    telemovelPaper: {
+        marginTop: 30,
+        marginLeft: 30,
+        paddingLeft: 20,
+        padddingTop: 30,
+        marginBottom: 50,/* OBS. Coloquei esse margin bottom apenas pra abrir espaço no final da div
+                            porque o backgroundColor do root estava pequeno na tela. Foi só pra dar mais
+                            espaço de cor na tela.  */
+    },
+    telemovelNumber: {
+        fontWeight: "bold",
+    },
+    logoZap: {
+        marginTop: 23,
+        float: 'left',
+    },
+
+}));
 
 const Contacto = () => {
+
+    const classes = useStyles();
+
     return (
         <>
-            <h1>Contacto</h1>
+            <div className={classes.root}>
+
+                <Grid container spacing={1}>
+
+                    <Grid item xs={12}>
+
+                        <h3 className={classes.contacto}> Contacto </h3>
+                        <Paper className={classes.contacto}>
+
+                            Envie sua música, ou faça contacto através do número de telemóvel
+                      </Paper>
+                        <img className={classes.logoZap} src="/imagens/whatsApp.svg" />
+                        <Paper className={classes.telemovelPaper}>
+                            <p className={classes.telemovelNumber}>926 429 016</p>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
+            <div>
+
+            </div>
         </>
     )
 }
